19f2cd3ccf3784a9ea9e2571e375c1208c43ea49
Khuong19 <133074738+Khuong19@users.noreply.github.com>
2023-08-29 13:10:19 +0700

    ok

--- client/src/pages/Customizer.jsx
+++ client/src/pages/Customizer.jsx
@@ -7,10 +7,48 @@ import state from '../store'
 import { download } from '../assets'
 import { downloadCanvasToImage, reader } from '../config/helpers'
 import { EditorTabs, FilterTabs, DecalTpes } from '../config/constants'
-import { fadeAnimation, sideAnimation } from '../config/motion'
+import { fadeAnimation, sideAnimation, slideAnimation } from '../config/motion'
+import { AIPicker, ColorPicker, CustomButton, FilePicker, Tab } from '../components'
+
 const Customizer = () => {
+  const snap = useSnapshot(state)
   return (
-    <div>Customizer</div>
+    <AnimatePresence>
+      {!snap.intro && (
+        <>
+          <motion.div
+            key='custom'
+            className='absolute top-0 left-0 z-10'
+            {...slideAnimation('left')}
+          > 
+            <div className='flex items-center min-h-screen'>
+              <div className="editortabs-container tabs">
+                {EditorTabs.map((tab) => (
+                  <Tab
+                    key={tab.name}
+                    tab={tab}
+                    handleClick={() => {}}
+                  />
+                ))}
+              </div>
+            </div>
+          </motion.div>
+
+          <motion.div
+            className='absolute z-10 top-5 right-5'
+            {...fadeAnimation}
+          >
+            <CustomButton
+              type='filled'
+              title='Go Back'
+              handleClick={() => state.intro = true}
+              customStyles="w-fit px-4 py-2.5 foont-bold text-sm"
+            />
+
+          </motion.div>
+        </>
+      )}
+    </AnimatePresence>
   )
 }
 
